5e9a2b7d0c347eda6181791706076e5ecf19f769
Gustavo Freitas <freitasgustavos@hotmail.com>
2025-06-11 21:44:24 -0300

    finish project

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {}
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CertificadoService } from './_services/certificado.service';
+import { FooterComponent } from './_components/footer/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    NavbarComponent,
+    CommonModule,
+    BaseUiComponent,
+    RouterOutlet,
+    FooterComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
+
+  constructor(private certificadoService: CertificadoService) {}
+
+  ngOnInit(): void {
+    const certificados = localStorage.getItem('certificados');
+    this.certificadoService.certificados = certificados
+      ? JSON.parse(certificados)
+      : [];
+  }
+}
